c85388a863585e05ee0059a3f8b5d0e790dda89c
Sergii5854 <bondarenko.sergii.i@gmail.com>
2018-12-07 17:16:18 +0200

    decomposition

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-console.log("hello");
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const TodoList = () => {
+    return (
+        <ul>
+            <li>
+                Create todo list
+            </li>
+        </ul>
+    )
+}
+
+const AppHeader = () => {
+    return <h1> Todo List </h1>
+}
+
+const SearchPanel  = () => {
+    return <input type="text" placeholder="search "/>
+}
+
+const el = (
+    <div>
+        <AppHeader/>
+        <SearchPanel/>
+        <TodoList/>
+
+    </div>
+)
+
+ReactDOM.render(el,
+    document.getElementById('root'))
+
