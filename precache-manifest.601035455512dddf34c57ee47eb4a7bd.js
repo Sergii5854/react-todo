2bcfee3338f7a710d2f4c0fbc71c8b82fb1071b3
Sergii5854 <bondarenko.sergii.i@gmail.com>
2018-12-10 12:04:50 +0200

    Updates

---
+++ precache-manifest.601035455512dddf34c57ee47eb4a7bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "755448a9d1e4650b31de",
+    "url": "/Sergii5854/react-todo/static/js/runtime~main.755448a9.js"
+  },
+  {
+    "revision": "800b3e292a228c7adc48",
+    "url": "/Sergii5854/react-todo/static/js/main.800b3e29.chunk.js"
+  },
+  {
+    "revision": "c26583f2bc635d6b5c6e",
+    "url": "/Sergii5854/react-todo/static/js/1.c26583f2.chunk.js"
+  },
+  {
+    "revision": "800b3e292a228c7adc48",
+    "url": "/Sergii5854/react-todo/static/css/main.cab40e6b.chunk.css"
+  },
+  {
+    "revision": "9476f9d818750b218e3bab49744b4761",
+    "url": "/Sergii5854/react-todo/index.html"
+  }
+];
